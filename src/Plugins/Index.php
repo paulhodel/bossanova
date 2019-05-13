9d9315e6b4ff926863053c1218204a535ff04bf5
paulhodel <paulhodel@DESKTOP-HVAO2Q0>
2019-05-13 18:29:42 +0100

    Index module

---
+++ src/Plugins/Index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * (c) 2013 Bossanova PHP Framework 4
+ * https://bossanova.uk/php-framework
+ *
+ * @category PHP
+ * @package  Bossanova
+ * @author   Paul Hodel <paul.hodel@gmail.com>
+ * @license  The MIT License (MIT)
+ * @link     https://bossanova.uk/php-framework
+ *
+ * Index module
+ */
+namespace bossanova\Plugins;
+
+use bossanova\Render\Render;
+use bossanova\Database\Database;
+
+class Index
+{
+    public function run()
+    {
+        // Possible username
+        if (Render::$notFound == 1) {
+            // Default module
+            if (file_exists("modules/Index/Index.php")) {
+                // Locate route
+                $realpath = implode('/', Render::$urlParam);
+
+                if (! $realpath) {
+                    $filename = "modules/Index/views/index.html";
+                } else {
+                    $filename = "modules/Index/views/" . $realpath . ".html";
+                }
+
+                if (file_exists($filename)) {
+                    Render::$configuration['module_name'] = "Index";
+                    Render::$configuration['view_name'] = (! $realpath) ? 'index' : $realpath;
+                }
+            }
+        }
+    }
+}

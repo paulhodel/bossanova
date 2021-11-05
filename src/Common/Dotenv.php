b72d07d738d5b18d2a003a3761f9f2249d4170f2
= <paul.hodel@gmail.com>
2021-11-05 22:29:18 +0000

    Dotenv

---
+++ src/Common/Dotenv.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * (c) 2013 Bossanova PHP Framework 5
+ * https://bossanova.uk/php-framework
+ *
+ * @category PHP
+ * @package  Bossanova
+ * @author   From the internet
+ * @license  The MIT License (MIT)
+ * @link     https://bossanova.uk/php-framework
+ *
+ * Configuration
+ */
+namespace bossanova\Common;
+
+class Dotenv
+{
+    static public function get($path)
+    {
+        if (! is_readable($path)) {
+            throw new \RuntimeException(sprintf('%s file is not readable', $path));
+        }
+
+        if (! file_exists($path)) {
+            throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
+        }
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (! array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+                if (substr($value, 0, 1) == "'" || substr($value, 0, 1) == '"') {
+                    $value = substr($value, 1);
+                }
+                if (substr($value, -1, 1) == "'" || substr($value, -1, 1) == '"') {
+                    $value = substr($value, 0,-1);
+                }
+                $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
+}

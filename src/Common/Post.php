0dd6f35fa50c594e15b24850c531bc80620cdbf5
paulhodel <86228233+paulhodel@users.noreply.github.com>
2022-10-16 10:15:09 +0100

    Update Post.php

--- src/Common/Post.php
+++ src/Common/Post.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * (c) 2013 Bossanova PHP Framework 5
- * https://bossanova.uk/php-framework
- *
- * @category PHP
- * @package  Bossanova
- * @author   Paul Hodel <paul.hodel@gmail.com>
- * @license  The MIT License (MIT)
- * @link     https://bossanova.uk/php-framework
- *
- * Incoming Post Variables
- */
+
 namespace bossanova\Common;
 
 trait Post

6031367245f8e0398b9dfa476069a8137eeb2955
paulhodel <86228233+paulhodel@users.noreply.github.com>
2022-10-16 10:15:48 +0100

    Update Database.php

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * (c) 2013 Bossanova PHP Framework 5
- * https://bossanova.uk/php-framework
- *
- * @category PHP
- * @package  Bossanova
- * @author   Paul Hodel <paul.hodel@gmail.com>
- * @license  The MIT License (MIT)
- * @link     https://bossanova.uk/php-framework
- *
- * Database Library
- */
+
 namespace bossanova\Database;
 
 use bossanova\Mail\Mail;
